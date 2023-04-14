db5a42ed0a2cb16cce1c7bfeb08da7a64b769b0b
zsakvo <gdwqsp@gmail.com>
2023-04-15 06:55:39 +0800

    fix: monet theme color error

--- .cicada/my-pipeline.ts
+++ .cicada/my-pipeline.ts
@@ -1,20 +1,39 @@
-import { Job, Pipeline } from "https://deno.land/x/cicada@v0.1.34/lib.ts";
+import { Job, Pipeline } from 'https://deno.land/x/cicada@v0.1.34/lib.ts'
 
 const job = new Job({
-  name: "My First Job",
-  image: "ubuntu:22.04",
+  name: 'My First Job',
+  image: 'ubuntu:22.04',
   steps: [
     {
-      name: "Print a message",
-      run: "echo Hello, world!",
+      name: 'Print a message',
+      run: 'echo Hello, world!',
     },
     {
-      name: "Run a js function",
+      name: 'Run a js function',
       run: () => {
-        console.log("Hello from js");
+        console.log('Hello from js')
       },
     },
   ],
-});
+})
 
-export default new Pipeline([job]);
+const jobEnv = new Job({
+  name: 'Init Env.',
+  image: 'ubuntu:22.04',
+  steps: [
+    {
+      name: 'install snapd',
+      run: 'apt update&&apt install snapd -y',
+    },
+    {
+      name: 'install flutter',
+      run: 'snap install flutter --classic',
+    },
+    {
+      name: 'print env',
+      run: 'flutter sdk-path',
+    },
+  ],
+})
+
+export default new Pipeline([jobEnv])
