dee767093e8ba29a37aa78033f8dee1fc1ad930a
zsakvo <gdwqsp@gmail.com>
2024-04-22 23:18:33 +0800

    fix: 修正搜索作者图标

--- lib/screen/detail/detail_screen.dart
+++ lib/screen/detail/detail_screen.dart
@@ -52,8 +52,7 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
       listener() {
         double pos = scrollController.position.pixels;
         showAppBarTitle.value = pos > 96.0;
-        showExtendFab.value = scrollController.position.userScrollDirection ==
-            ScrollDirection.forward;
+        showExtendFab.value = scrollController.position.userScrollDirection == ScrollDirection.forward;
       }
 
       scrollController.addListener(listener);
@@ -63,12 +62,10 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
     }, [scrollController]);
     return Scaffold(
         extendBodyBehindAppBar: true,
-        backgroundColor:
-            Theme.of(context).extension<ExtendColors>()!.elevationBackground,
+        backgroundColor: Theme.of(context).extension<ExtendColors>()!.elevationBackground,
         body: detail.intro == null
             ? const Center(
-                child: SizedBox(
-                    width: 96, height: 3, child: LinearProgressIndicator()),
+                child: SizedBox(width: 96, height: 3, child: LinearProgressIndicator()),
               )
             : CustomScrollView(
                 controller: scrollController,
@@ -91,36 +88,40 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                           //     Theme.of(context).colorScheme.secondary,
                           //     BlendMode.srcIn),
                         )),
-                    backgroundColor: Theme.of(context)
-                        .extension<ExtendColors>()!
-                        .elevationBackground,
+                    backgroundColor: Theme.of(context).extension<ExtendColors>()!.elevationBackground,
                     actions: [
                       Padding(
-                        padding: const EdgeInsets.only(right: 4, left: 4),
-                        child: FilledButton.tonalIcon(
-                            onPressed: () {
-                              GoRouter.of(context)
-                                  .push("/search", extra: detail.author);
-                            },
-                            label: const Text("搜索作者"),
-                            icon: SvgPicture.asset(
-                              "assets/svg/ic_topbar_person_search.svg",
-                              width: 18,
-                            )),
-                      )
+                          padding: const EdgeInsets.only(right: 4, left: 4),
+                          child: IconButton(
+                              onPressed: () {
+                                GoRouter.of(context).push("/search", extra: detail.author);
+                              },
+                              icon: SvgPicture.asset(
+                                "assets/svg/ic_topbar_person_search.svg",
+                                width: 18,
+                              ))
+                          // FilledButton.tonalIcon(
+                          //     onPressed: () {
+                          //       GoRouter.of(context)
+                          //           .push("/search", extra: detail.author);
+                          //     },
+                          //     label: const Text("搜索作者"),
+                          //     icon: SvgPicture.asset(
+                          //       "assets/svg/ic_topbar_person_search.svg",
+                          //       width: 18,
+                          //     )),
+                          )
                     ],
                     title: Text(
                       detail.name,
                       style: TextStyle(
                           fontSize: 18,
-                          color: showAppBarTitle.value
-                              ? Theme.of(context).colorScheme.onBackground
-                              : Colors.transparent),
+                          color:
+                              showAppBarTitle.value ? Theme.of(context).colorScheme.onBackground : Colors.transparent),
                     ),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 20),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                     sliver: SliverToBoxAdapter(
                       child: Row(
                         children: [
@@ -144,34 +145,23 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                                     overflow: TextOverflow.ellipsis,
                                     style: TextStyle(
                                         fontWeight: FontWeight.w600,
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onBackground,
+                                        color: Theme.of(context).colorScheme.onBackground,
                                         fontSize: 18),
                                   ),
                                   Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8, bottom: 8),
+                                    padding: const EdgeInsets.only(top: 8, bottom: 8),
                                     child: Text(
                                       "${detail.author!}\t/\t${detail.status!}",
                                       maxLines: 1,
                                       overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .secondary,
-                                          fontSize: 14),
+                                      style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 14),
                                     ),
                                   ),
                                   Text(
                                     "更新时间：${detail.lastUpdate!}",
                                     maxLines: 1,
                                     overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .secondary,
-                                        fontSize: 14),
+                                    style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 14),
                                   ),
                                 ]),
                           ))
@@ -180,8 +170,7 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                     ),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 16),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
                     sliver: SliverToBoxAdapter(
                         child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -191,36 +180,22 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                           highlightColor: Colors.transparent,
                           onTap: () {
                             isFav
-                                ? ref
-                                    .read(myBooksProvider.notifier)
-                                    .delBook(detail.aid)
-                                : ref
-                                    .read(myBooksProvider.notifier)
-                                    .addBook(detail);
+                                ? ref.read(myBooksProvider.notifier).delBook(detail.aid)
+                                : ref.read(myBooksProvider.notifier).addBook(detail);
                           },
                           child: Column(children: [
                             SvgPicture.asset(
-                              isFav
-                                  ? "assets/svg/ic_btn_like.svg"
-                                  : "assets/svg/ic_btn_like_line.svg",
+                              isFav ? "assets/svg/ic_btn_like.svg" : "assets/svg/ic_btn_like_line.svg",
                               width: 20,
                               colorFilter: ColorFilter.mode(
-                                  Theme.of(context)
-                                      .colorScheme
-                                      .secondary
-                                      .withOpacity(0.7),
-                                  BlendMode.srcIn),
+                                  Theme.of(context).colorScheme.secondary.withOpacity(0.7), BlendMode.srcIn),
                             ),
                             Padding(
                               padding: const EdgeInsets.only(top: 4),
                               child: Text(
                                 isFav ? "已在书架" : "加入书架",
                                 style: TextStyle(
-                                    fontSize: 12,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary
-                                        .withOpacity(0.7)),
+                                    fontSize: 12, color: Theme.of(context).colorScheme.secondary.withOpacity(0.7)),
                               ),
                             )
                           ]),
@@ -235,19 +210,13 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                             SvgPicture.asset(
                               "assets/svg/ic_btn_share.svg",
                               width: 20,
-                              colorFilter: ColorFilter.mode(
-                                  Theme.of(context).colorScheme.secondary,
-                                  BlendMode.srcIn),
+                              colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
                             ),
                             Padding(
                               padding: const EdgeInsets.only(top: 4),
                               child: Text(
                                 "分享本书",
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary),
+                                style: TextStyle(fontSize: 12, color: Theme.of(context).colorScheme.secondary),
                               ),
                             )
                           ]),
@@ -262,19 +231,13 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                             SvgPicture.asset(
                               "assets/svg/ic_btn_download.svg",
                               width: 20,
-                              colorFilter: ColorFilter.mode(
-                                  Theme.of(context).colorScheme.secondary,
-                                  BlendMode.srcIn),
+                              colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
                             ),
                             Padding(
                               padding: const EdgeInsets.only(top: 4),
                               child: Text(
                                 downloadProgress.data!,
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary),
+                                style: TextStyle(fontSize: 12, color: Theme.of(context).colorScheme.secondary),
                               ),
                             )
                           ]),
@@ -283,8 +246,7 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                     )),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     sliver: SliverToBoxAdapter(
                         child: Divider(
                       height: 2,
@@ -293,13 +255,11 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                     )),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     sliver: SliverToBoxAdapter(child: Text(detail.intro!)),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     sliver: SliverToBoxAdapter(
                         child: Divider(
                       height: 2,
@@ -308,15 +268,12 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                     )),
                   ),
                   SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     sliver: SliverToBoxAdapter(
                         child: Text(
                       "目录：共${detail.catalog!.length}章",
                       style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.onBackground),
+                          fontSize: 16, fontWeight: FontWeight.w500, color: Theme.of(context).colorScheme.onBackground),
                     )),
                   ),
                   SliverList.builder(
@@ -327,12 +284,10 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                           // ref
                           //     .read(homeProvider.notifier)
                           //     .jumpToIndex(bookItem, index);
-                          GoRouter.of(context).push(
-                              "/reader/${detail.name}/${detail.aid}/$index");
+                          GoRouter.of(context).push("/reader/${detail.name}/${detail.aid}/$index");
                         },
                         child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 12),
+                            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
@@ -340,19 +295,11 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
                                   chapter.name,
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onBackground,
-                                      fontSize: 16),
+                                  style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 16),
                                 ),
                                 Text(
                                   "cid-${chapter.cid}",
-                                  style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary,
-                                      fontSize: 12),
+                                  style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 12),
                                 )
                               ],
                             )),
@@ -366,8 +313,7 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
             ? null
             : FloatingActionButton.extended(
                 onPressed: () {
-                  GoRouter.of(context)
-                      .push("/reader/${detail.name}/${detail.aid}/-1");
+                  GoRouter.of(context).push("/reader/${detail.name}/${detail.aid}/-1");
                 },
                 label: AnimatedContainer(
                   duration: const Duration(milliseconds: 180),
